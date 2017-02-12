1d596b0284076c75a8cc2ccc5d556710fe3c4ee6
sergeydaub <sergeydaub@gmail.com>
2017-02-12 19:06:59 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import Game from './controls/game/game';
+
+import './index.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('container')
+);
+
